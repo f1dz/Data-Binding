218c2f596bc93d23b838ca5d4e3443f1105ce5bf
Khofidin <offiedz@gmail.com>
2019-03-03 07:00:19 +0700

    C. Data Binding Basic

--- app/src/main/java/com/iteqno/databinding/MainActivity.java
+++ app/src/main/java/com/iteqno/databinding/MainActivity.java
@@ -1,13 +1,25 @@
 package com.iteqno.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.iteqno.databinding.databinding.ActivityMainBinding;
+import com.iteqno.databinding.model.User;
+
 public class MainActivity extends AppCompatActivity {
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        user = new User();
+        user.setName("Khofidin");
+        user.setEmail("offiedz@gmail.com");
+
+        binding.setVm(user);
     }
 }
